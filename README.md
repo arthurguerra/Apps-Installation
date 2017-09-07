70b9be8f1421a4b3eb10d585d960a171c53e7665
Arthur Guerra <guerra@Arthurs-MacBook-Pro.local>
2017-09-07 07:36:17 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Applications Setup
+
+This repo is a shortcut for apps installation on brand new environments.
+
+Just run `./apps-setup.sh` to begin the installation of many useful programs
+on your OS. After the script is done, everything will be correctly configured. 
+
+It also sets `zsh` as your default bash.
+
+No need to manually download packages for each application again =)
+
+Thanks to [Homebrew](https://brew.sh) and 
+[Homebrew Cask](https://github.com/caskroom/homebrew-cask) that do all the heavy 
+work for us!
+
+https://gillesfabio.github.io/homebrew-cask-homepage/
+
+http://sourabhbajaj.com/mac-setup/
---
+++ apps-setup.sh
@@ -0,0 +1,39 @@
+# OSX Applications Setup (dowload and install applications via brew cask)
+
+echo 'Installing homebrew...'
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo 'Homebrew installed'
+
+brew install vim \
+             zsh \
+             zsh-completions \
+             zsh-autosuggestions \
+             zsh-syntax-highlighting \
+             tree \
+             wget \
+             pyenv
+
+pip install --upgrade setuptools
+pip install --upgrade pip
+
+# oh-my-zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+chsh -s /usr/local/bin/zsh
+
+echo 'Installing packages via brew cask...'
+
+brew cask install google-chrome \
+                  git \
+                  java \
+                  firefox \
+                  iterm2 \
+                  docker \
+                  pycharm-ce \
+                  intellij-idea-ce \
+                  atom \
+                  vlc \
+                  postman \
+                  skype \
+                  spotify
+
+echo '\nPackages installed.'
